a053ad2f7b46e13407db583c111c689115afbb5d
Alexander Boehm <alex.boehm89@gmail.com>
2018-03-21 16:10:50 +0100

    Player creation

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,37 @@
 
 int main (int argc, char **argv)
 {
+    /***** Ask user for amont of players *****/
+    std::cout << "Please enter the number of players!" << std::endl;
+    // Define necessary variables
+    int number_of_players;
+    std::vector<std::string> name_of_players;
+    std::vector<Player> players;
+
+    std::cin >> number_of_players;
+    if (number_of_players > 0) {
+        for (int idx = 1; idx != number_of_players + 1; idx++) {
+            std::string player_name;
+            std::cout << "Please enter the name for player " << idx << std::endl;
+            std::cin >> player_name;
+            name_of_players.push_back(player_name);
+            Player player(player_name);
+            players.push_back(player);
+            std::cout << "Created player " << player_name << " !" << std::endl;
+        }
+    } else {
+        std::cout << "You entered an invalid number of players! Exit The Game..." << std::endl;
+        return 1;
+    }
+
+
     std::cout << "########## Initializing The Game! ########## " << std::endl;
     std::cout << "Creating deck ..." << std::endl;
     Deck deck(98);
-    //deck.shuffle();
-    deck.show();
+    deck.shuffle();
     std::cout << "Creating stacks ..." << std::endl;
     Stack stack_up_1(1, "up");
     Stack stack_up_2(1, "up");
     Stack stack_down_1(100, "down");
     Stack stack_down_2(100, "down");
-
-    std::cout << "Creating player ..." << std::endl;
-    Player player("player1");
-    player.show_name();
-    player.draw(deck, 5);
-    player.show_remaining_cards();
-    player.play_card(stack_up_1, 2);
-    player.show_remaining_cards();
-    stack_up_1.show();
 }
